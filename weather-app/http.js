6fbd3ed6d5f62767ad2974748058ba01fec4d2c5
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-04-27 11:36:33 +0530

    updated

--- weather-app/http.js
+++ weather-app/http.js
@@ -2,7 +2,8 @@ const request =  require('request')
 
 const url = 'http://api.weatherstack.com/current?access_key=85da9ec8ae05d84156692ee070688822&query=37.8267,-122.4233'
 
-request({ url: url }, (error, response) => {
+// url  ==== { url: url} 
+request( url, (error, response) => {
     //console.log(response)
     const data = JSON.parse(response.body)
     console.log(data.current)
