16409311b15e4e714861db5b32b0f0907473efa5
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-04-27 12:23:40 +0530

    asynchrounous

---
+++ weather-app/http2.js
@@ -0,0 +1,12 @@
+const request =  require('request')
+
+const url = 'http://api.weatherstack.com/current?access_key=85da9ec8ae05d84156692ee070688822&query=37.8267,-122.4233&units=f'
+
+// simply converting the string into object use lowwer json only
+request( { url: url, json: true }, (error, response) => {
+    console.log(response.body.current)  
+    console.log(response.body.request)   // only print about request
+    console.log(response.body.location)  // only prints about the location 
+
+    console.log(response.body.current.weather_descriptions[0] + ', we are in ' + response.body.location.country + ' present temperature is ' + response.body.current.temperature)
+})  
