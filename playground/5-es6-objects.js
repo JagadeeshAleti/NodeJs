0005c7612ff8d1392d0aa47404f8689efaa8b143
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-04-29 10:06:38 +0530

    ES^objects

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,37 @@
+// object property shorthand
+const name = 'Andrew'
+const userAge = 27
+
+const user = {
+    name,
+    age: userAge,
+    location: 'New York'
+}
+
+console.log(user)
+
+
+//Object destructuring
+const product = {
+    label: 'Red NoteBook',
+    price: 3,
+    stock: 201,
+    salePrice: undefined,
+}
+
+// const label = product.label
+// const stock = product.stock
+
+const {label, stock: propertyStock, rating = 5} = product // if the rating is not in the product then we can assign default value otherwise if it exist int he object the value on the oject will be printed
+
+console.log(label, label, propertyStock, rating)
+
+
+// const transaction = (type, product) => {
+//     console.log(type, product.label, product.stock)
+// }
+const transaction = (type, {label, stock}) => {
+    console.log(type, label, stock)
+}
+
+transaction('order', product)
