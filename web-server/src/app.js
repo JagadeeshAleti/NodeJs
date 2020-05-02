eb26ea5ebf4c4681491ecf15c57027a46e4d3070
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-05-02 12:44:06 +0530

    Advanced Teplates : partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,32 +1,38 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
+   
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
         title: 'weather',
-        name: 'jagadeesh'
+        name: 'Andrew'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About me',
-        name: 'Andrew'
+        name: 'Jagadeesh'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpfulText' 
+        helpText: 'This is some helpfulText',
+        title: 'Help',
+        name: 'Jagadeesh'
     })
 })
 
