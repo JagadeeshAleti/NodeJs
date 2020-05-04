68df3207f0ae477acc4c8af3ddd7d6408b09e76d
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-05-04 10:51:16 +0530

    Endpoint

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
    
@@ -42,12 +44,25 @@ app.get('/weather', (req, res) => {
             error: 'You must enter a address'
         })
     }
-    
-    res.send({
-        forecast: 'sunny',
-        location: 'India',
-        address: req.query.address
+
+    geocode(req.query.address, (error, { latitude, longitude, location}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                    return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location, 
+                address: req.query.address
+            }) 
+        }) 
     })
+
 })
 
 app.get('/products', (req, res) => {    
