2ad29761b5a13a3d91f4a7fdf58632bf58650920
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-05-08 10:42:52 +0530

    refactoring app

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=85da9ec8ae05d84156692ee070688822&query=' + longitude + ',' + latitude + '&units=f'
 
-    request({ url: url, json: true}, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Check ypur internet connectivity', undefined)
-        } else if (response.body.error) {
-            callback('Unable to find location', undefined)
+        } else if (body.error) {
+            callback('Check URL once again', undefined)
         } else {
-            callback(undefined, 'Currentl we are in ' + response.body.location.name + 'and temperature is ' + response.body.current.temperature + ' by the temperature its like  ' + response.body.current.weather_descriptions[0])
+            callback(undefined, 'Currentl we are in ' + body.location.name + 'and temperature is ' + body.current.temperature + ' by the temperature its like  ' + body.current.weather_descriptions[0])
        }
     })
 }
