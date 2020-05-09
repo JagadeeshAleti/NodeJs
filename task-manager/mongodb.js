61ea12a522fe48889dcd6e1613cca753c3a354ad
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-05-09 20:39:47 +0530

    All about object IDs

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,10 +1,18 @@
 // CRUD create read update delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+            
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id.id.length)
+console.log(id.toHexString().length)
+//console.log(id.getTimestamp()) ===>> Give the time when the id was created by the MongoBD server
 
 MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client) => {
     if (error) {
@@ -13,49 +21,49 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
 
     const db = client.db(databaseName)
     
-    db.collection('users').insertOne({
-        name: 'Andrew',
-        age: 27
-    }, (error, result) => {
-        if (error) {
-            return console.log('Unable to insert user')
-        }
+    // db.collection('users').insertOne({
+    //     name: 'Vikram',
+    //     age: 26
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
 
-        console.log(result.ops)
-    })
+    //     console.log(result.ops)
+    // })
 
-    db.collection('users').insertMany([
-        {
-            name: 'Jen',
-            age: 28
-        }, {
-            name: 'Sunther',
-            age: 27 
-        }
-    ], (error, result) => {
-            if (error) {
-                return console.log('Unable to insert documents')
-            }
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     }, {
+    //         name: 'Sunther',
+    //         age: 27 
+    //     }
+    // ], (error, result) => {
+    //         if (error) {
+    //             return console.log('Unable to insert documents')
+    //         }
 
-            console.log(result.ops)
-    })
+    //         console.log(result.ops)
+    // })
 
-    db.collection('tasks').insertMany([
-        {
-            description: 'Clean the House',
-            completed: true
-        },{
-            description: 'Renew inspection',
-            completed: false
-        },{
-            description: 'Pot plants',
-            completed: false
-        }
-    ], (error, result) => {
-        if (error) {
-            return console.log('Unable to insert tasks!!')
-        }
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'Clean the House',
+    //         completed: true
+    //     },{
+    //         description: 'Renew inspection',
+    //         completed: false
+    //     },{
+    //         description: 'Pot plants',
+    //         completed: false
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert tasks!!')
+    //     }
 
-        console.log(result.ops)
-    })
+    //     console.log(result.ops)
+    // })
 })
