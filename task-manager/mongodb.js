f2159ba3c2682713c5f70ae6d3b7f67c57e98cdd
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-05-10 19:43:07 +0530

    deleting documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -16,28 +16,19 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
 
     const db = client.db(databaseName)
   
-    db.collection('users').updateOne({
-        _id: new ObjectID('5eb78bf141f8de0d9ebd1434')
-    }, {
-        $inc: {
-            age: 1
-        }
+    // db.collection('users').deleteMany({
+    //     age: 27
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })  
+
+    db.collection('tasks').deleteOne({
+        description: 'Clean the House'
     }).then((result) => {
         console.log(result)
     }).catch((error) => {
         console.log(error)
     })
-
-    db.collection('tasks').updateMany({
-        completed: false
-    }, {
-        $set: {
-            completed: true
-        }
-    }).then((result) => {
-        console.log('No.of documents updated : ' + result.modifiedCount)
-    }).catch((error) => {
-        console.log(error)
-    })
-
 })
