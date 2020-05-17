02c329f5e20f1ac2eac5db9691f7ca1334a0f290
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-05-17 10:28:48 +0530

    async-await

--- task-manager/playground/promise-chaining2.js
+++ task-manager/playground/promise-chaining2.js
@@ -3,10 +3,22 @@ const Task = require('../src/models/task')
 
 //5eba31fcd5a47d0eb88e33fc
 
-Task.findByIdAndDelete('5ebb757d663961110afe5f72').then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: false })
-}).then((result) => {
+// Task.findByIdAndDelete('5ebb757d663961110afe5f72').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskCount('5ebb7451eb99ee0f2f8b57ad').then((result) => {
     console.log(result)
 }).catch((e) => {
     console.log(e)
